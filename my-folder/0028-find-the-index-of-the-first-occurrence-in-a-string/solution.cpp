33e6ea5874984fd0aebec5ba237c7bf87d261f09
Ismail Muhammed <ismailmd.dev@gmail.com>
2024-06-16 11:39:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

---
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+    
+    for (int i = 0; i < haystack.size(); ++i) {
+        if (haystack[i] == needle[0]) {
+            if (haystack.substr(i, needle.size()) == needle) {
+                return i;
+            }
+            else {
+                continue;
+            }
+        }
+    }
+    return -1;
+   
+}
+    
+};
