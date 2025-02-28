809c4e5b078d70b2b77b70d142936da793f5d162
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-02-28 12:39:00 +0000

    [LeetCode Sync] Runtime - 17 ms (53.58%), Memory - 26.1 MB (48.52%)

--- my-folder/0049-group-anagrams/solution.cpp
+++ my-folder/0049-group-anagrams/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> anagrams{};
-        unordered_map<string,int> savedtotals;
-        for(string &x : strs){
-                string a = x;
-                sort(x.begin(),x.end());
-                if(savedtotals[x]){
-                    anagrams[savedtotals[x]-1].push_back(a);
-                } else {
-                    savedtotals[x] = anagrams.size()+1;
-                    vector<string> t {a};
-                    anagrams.push_back(t);
-                }
-            }        
-            return anagrams;
+        unordered_map<string,vector<string>> mapz;
+        for(int i = 0;i<strs.size();++i){
+            string sorted = strs[i];
+            sort(sorted.begin(),sorted.end());
+            mapz[sorted].push_back(strs[i]);
+        }
+        vector<vector<string>> ans;
+        for(auto i : mapz){
+            ans.push_back(i.second);
+        }
+        return ans;
     }
 };
