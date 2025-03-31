48bfae755d2a31bf2d97989078433a2544a52aab
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-03-31 13:05:26 +0000

    [LeetCode Sync] Runtime - 1 ms (46.14%), Memory - 9.9 MB (86.97%)

--- my-folder/0151-reverse-words-in-a-string/solution.cpp
+++ my-folder/0151-reverse-words-in-a-string/solution.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
 string reverseWords(string s) {
-    vector<string> t;
-    string actualWord="";
-    s= s+ ' ';
-    for(int i = 0;i<s.size();++i){
-        if(s[i] == ' '){
-            if(!actualWord.empty())
-            t.push_back(actualWord);
-            actualWord = "";
-            continue;
-        }
-        else{
-            actualWord.push_back(s[i]);
-        }
+    if(s == "")
+        return s;
+    int n = s.size();
+    int i=0;
+    int j=0;
+    while(j < n){
+        while(j < n && s[j] == ' ') j++;
+        while(j < n && s[j] != ' ') s[i++] = s[j++];
+        if(j < n) s[i++] = ' ';
     }
-    string sentence;
-    for(int i = t.size()-1;i>=0;--i){
-        if(i == 0){
-            sentence.append(t[0]);
-            break;
-        }
-        else{
-            sentence.append(t[i] + " ");
-                    }
+
+    if (i > 0 && s[i-1] == ' ')
+        i--;
+
+    s.resize(i);
+    int right = s.size()-1;
+    int left = 0;
+    while(left < right){
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
     }
-    return sentence;
+
+    i = 0;
+    while(i < s.size()){
+        int j = i;
+        while(s[j] != ' ' && j < s.size()) ++j;
+        int in = j;
+        j--;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            ++i;
+            --j;
+        }
+        i = in+1;
+    }
+
+
+    return s;
 
 }
+
 };
