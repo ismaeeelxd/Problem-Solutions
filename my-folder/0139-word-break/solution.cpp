f112be2f2c9ce7f5d32cf9c8a93d8e1f59c009e1
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-07-16 11:36:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (99.88%)

--- my-folder/0139-word-break/solution.cpp
+++ my-folder/0139-word-break/solution.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
+    int memo[301];
     bool wordBreak(string s, vector<string>& wordDict) {
-        int memo[301];
         memset(memo,-1,sizeof(memo));
-        return solve(s,wordDict,0,memo);
+        return solve(s,wordDict,0);
     }
 
-    bool solve(string &s, vector<string> &wordDict,  int i, int(&memo)[301]){
-        if(i == s.size())
-            return true;
-        auto &ret = memo[i];
-        if(ret != -1)
-            return ret;
-        for(const auto &word : wordDict){
+    bool solve(string &s, vector<string>&wordDict, int i){
+        if(i == s.size()) return true;
+        if(memo[i] != -1) return memo[i];
+        for(const auto &word: wordDict){
+            if(i + word.size() > s.size()) continue;
             bool match = true;
-            if (i + word.size() > s.size()) continue; 
-            for(int j = 0;j<word.size();++j){
-                if(word[j] != s[i + j]){
+            int k = i;
+            for (int j = 0; j < word.size(); ++j,++k){
+                if(s[k] != word[j]){
                     match = false;
                     break;
-                }
+                } 
             }
-            if(match){
-                bool choice = solve(s,wordDict,i+word.size(),memo);
-                if(choice)
-                    return ret = true;
+            if(match && solve(s,wordDict,i+word.size())){
+                return memo[i] = true;
             }
         }
-        return ret = false;
+        return memo[i] = false;
     }
-
 };
