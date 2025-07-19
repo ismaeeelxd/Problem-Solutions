5ec00047476bfc5cdd6aefbb615827e52132a661
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-07-19 17:14:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (16.50%)

---
+++ my-folder/0367-valid-perfect-square/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+    
+      for(long long i = 0; i <= num; ++i){
+        if(i * i == num) return true;
+        if(i*i > num) return false;
+      }  
+      return 0;
+    }
+};
