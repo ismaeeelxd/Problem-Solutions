ccca1b36f225703602a38539b69d132a2dbf2360
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-08-01 11:45:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.6 MB (62.71%)

--- my-folder/0118-pascals-triangle/solution.cpp
+++ my-folder/0118-pascals-triangle/solution.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    ;
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> answer(numRows);
-        answer[0].resize(1);
-        answer[0][0] = 1;
-        for(int i = 1;i < numRows;++i){
-            answer[i].resize(i+1);
-            answer[i][0] = 1;
-            answer[i][i] = 1;
-            for(int j = 1; j < i; ++j)
-                answer[i][j] = answer[i-1][j-1] + answer[i-1][j];
+        vector<vector<int>> res;
+        res.push_back({1});
+        for(int i = 2; i <= numRows; ++i){
+            vector<int> p(i,0);
+            p[0] = 1;
+            p[i-1] = 1;
+            for(int j = 1; j < i - 1; ++j){
+                p[j] = res[i-2][j-1] + res[i-2][j];
+            }
+            res.push_back(p);
         }
-        return answer;
+        return res;
     }
-
-    void formRow(vector<vector<int>> &answer,int numRows){
-        answer[numRows-1].resize(numRows);
-        if(numRows == 1){
-            answer[0][0] = 1;
-            return;
-        }
-
-        answer[numRows-1][0] = 1;
-        answer[numRows-1][numRows-1] = 1;
-        
-        formRow(answer,numRows-1);
-
-        for(int i = 1; i< numRows - 1; ++i){
-            answer[numRows-1][i] = answer[numRows-2][i-1] + answer[numRows-2][i];
-        }
-
-    }
-
-    
 };
