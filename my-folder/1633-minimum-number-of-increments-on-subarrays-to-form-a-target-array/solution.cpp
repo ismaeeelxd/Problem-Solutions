67ef83ba0c302aebd9cef7dfed33eda372fdd05e
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-10-30 09:06:07 +0000

    [LeetCode Sync] Runtime - 1 ms (49.78%), Memory - 77.1 MB (22.38%)

---
+++ my-folder/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int total = target[0];
+        for(int i = 1; i < target.size(); ++i) {
+            if(target[i] - target[i-1] > 0) total+=(target[i] - target[i-1]);
+        }
+        return total;
+    }
+};
