8016909cef1a489c7befa342387b0e993324b63f
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-11-14 13:46:59 +0000

    [LeetCode Sync] Runtime - 1106 ms (5.33%), Memory - 91.3 MB (100.00%)

---
+++ my-folder/2625-increment-submatrices-by-one/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> grid(n,vector<int>(n,0));
+        for(auto query: queries) {
+            int row1 = query[0];
+            int col1 = query[1];
+            int row2 = query[2];
+            int col2 = query[3];
+            for(int i = row1; i <= row2; ++i) {
+                for(int j = col1; j <= col2; ++j){
+                    grid[i][j]++;
+                }
+            }
+        }
+        return grid;
+    }
+};
