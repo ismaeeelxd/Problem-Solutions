7c95848ca3a43b993357e636da42732ac2a02861
Ismail Muhammed <ismailmd.dev@gmail.com>
2025-11-25 07:06:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (80.28%)

---
+++ my-folder/1064-smallest-integer-divisible-by-k/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k == 2 || k == 5) return -1;
+        int rem = 0;
+        for(int l = 1; l <= k; ++l) {
+            rem = (rem * 10 + 1) % k;
+            if(rem == 0) return l;
+        }
+        return -1;
+    }
+};
