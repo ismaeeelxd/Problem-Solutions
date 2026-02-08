ed781e76b6048ec0591febba4dda9584dc23a963
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-02-08 08:32:39 +0000

    [LeetCode Sync] Runtime - 8 ms (5.18%), Memory - 43.7 MB (5.92%)

--- my-folder/0238-product-of-array-except-self/solution.cpp
+++ my-folder/0238-product-of-array-except-self/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> suffix(nums.size());
-        vector<int> prefix(nums.size());
-        vector<int> ans(nums.size());
-        prefix[0] = 1;
-        for(int i = 1;i<nums.size();++i){
-            prefix[i] = (prefix[i-1] * nums[i-1]);
+        vector<int> suffix(nums.size(), 1);
+        vector<int> prefix(nums.size(), 1);
+
+        for(int i = 1, j = nums.size() - 2; i < nums.size(); ++i, j--) {
+            prefix[i]*=(nums[i-1] * prefix[i-1]);
+            suffix[j]*=(nums[j+1] * suffix[j+1]);
+
         }
-        suffix[nums.size()-1] = 1;
-        for(int i = nums.size() - 2; i>=0;--i){
-            suffix[i] = (suffix[i+1] * nums[i+1]);
-        }
-        for(int i = 0;i<nums.size();++i){
-            ans[i] = prefix[i]*suffix[i];
-        }
-        return ans;
-    }
+
+        vector<int> res;
+        for(int i = 0; i < nums.size(); ++i) res.push_back(suffix[i] * prefix[i]);
+        return res;
+    }   
 };
