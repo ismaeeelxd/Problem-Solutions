20546a8b00f18ff4bf5d8fc3348892806350d107
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-02-16 11:07:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (88.30%)

--- my-folder/0020-valid-parentheses/solution.cpp
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-   bool isValid(string s) {
-    stack<char> arr;
-    if (s.size() % 2 != 0) {
-        return false;
-    }
-    for (int i = 0;i<s.size();++i) {
-        if (s[i] == '(' || s[i] == '[' || s[i] =='{')
-            arr.push(s[i]);
-        else if(s[i] ==')') {
-            if (i == 0 || arr.empty() || arr.top() != '(')
-                return false;
-            
-            
-            arr.pop();
-        }
-        else if(s[i] == ']') {
-                if (i == 0 || arr.empty() || arr.top() != '[')
-                    return false;
-                arr.pop();
-            }
-        else if(s[i] == '}') {
-                    if (i == 0 || arr.empty() || arr.top() != '{')
-                        return false;
-                    arr.pop();
+    bool isValid(string s) {
+                stack<char> st;
+                for(auto x : s) {
+                    if(x == '{' || x == '(' || x == '[') st.push(x);
+                    else {
+                        switch(x) {
+                            case '}':
+                                if(!st.empty() && st.top() == '{') st.pop();
+                                else return false;
+                                break;
+                            case ')':
+                                if(!st.empty() && st.top() == '(') st.pop();
+                                else return false;
+                                break;
+                            case ']':
+                                if(!st.empty() && st.top() == '[') st.pop();
+                                else return false;
+                                break;
+                        }
+                    }
                 }
-
+                return st.empty();
     }
-    return arr.empty();
-}
 };
-
-
-
-
