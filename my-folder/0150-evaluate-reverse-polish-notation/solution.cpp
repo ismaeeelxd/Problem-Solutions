dab7dfe88f3008f292a7938056c14905c7a68cb0
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-02-17 22:24:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.9 MB (85.96%)

--- my-folder/0150-evaluate-reverse-polish-notation/solution.cpp
+++ my-folder/0150-evaluate-reverse-polish-notation/solution.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
+    int convertStringToInteger(string token) {
+        bool isNegative = false;
+
+        if(token[0] == '-' && token.size() > 1) isNegative = true;
+        else {
+            if(!isdigit(token[0])) return INT_MIN;
+        }
+        int number = 0;
+        int multipler = 1;
+        // 13445
+        for(int i = token.size() - 1; i >= (isNegative ? 1 : 0); --i) {
+            number+=(multipler*((int)(token[i] - '0')));
+            multipler*=10;
+        }
+        return isNegative ? number*=-1 : number;
+    }
+
+    void printStack(stack<int> s) {
+        while(!s.empty()) {
+            cout << s.top() << " ";
+            s.pop();
+        }
+        cout << endl;
+    }
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
-        for(auto token : tokens) {
-            if(token == "/") {
-                int num1 = s.top();
-                s.pop();
-                int num2 = s.top();
-                s.pop();
-                s.push(num2 / num1);
-            } else if(token == "*") {
-                int num1 = s.top();
-                s.pop();
-                int num2 = s.top();
-                s.pop();
-                s.push(num2 * num1);
-
-            } else if(token == "+") {
-                int num1 = s.top();
-                s.pop();
-                int num2 = s.top();
-                s.pop();
-                s.push(num2 + num1);
-
-            } else if(token == "-") {
-                int num1 = s.top();
-                s.pop();
-                int num2 = s.top();
-                s.pop();
-                s.push(num2 - num1);
+        for (int i = 0; i < tokens.size(); ++i) {
+            string token = tokens[i];
+            int number = convertStringToInteger(token);
+            if (number != INT_MIN) { // -11
+                s.push(number);
+                continue;
             }
-            else {
-                s.push(stoi(token));
-            }
+
+            int number_1 = s.top();
+            s.pop();
+            int number_2 = s.top();
+            s.pop();
+
+            if (token == "+")
+                s.push(number_1 + number_2);
+
+            if (token == "-")
+                s.push(number_2 - number_1);
+
+            if (token == "*")
+                s.push(number_2 * number_1);
+
+            if (token == "/")
+                s.push(number_2 / number_1);
         }
-            return s.top();
+        return s.top();
     }
 };
