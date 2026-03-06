135cbf6559ec344e26e4f33179bda7269e624cfc
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-03-06 11:29:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (97.13%)

---
+++ my-folder/1910-check-if-binary-string-has-at-most-one-segment-of-ones/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int i = 0;
+        while(i < s.size() && s[i] == '1') ++i;
+        if(i >= s.size()) return true;
+        while(i < s.size() && s[i] == '0') ++i;
+        if(i >= s.size()) return true;
+        
+
+        
+
+        return false;
+    }
+};
