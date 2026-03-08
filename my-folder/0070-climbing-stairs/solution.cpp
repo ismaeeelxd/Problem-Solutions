726be62edf902ac329adfab2afe282ae14729f02
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-03-08 18:26:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (16.99%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
+
+    int getDistinctWays(int n, vector<int>& mem) {
+        if(n == 0) {
+            return 1;
+        } else if(n < 0) return 0;
+        auto &ret = mem[n];
+        if(ret != -1) return mem[n];
+        int opt1 = getDistinctWays(n-1, mem);
+        int opt2 = getDistinctWays(n-2, mem);
+        return mem[n] =  opt1+opt2;
+
+    }
     int climbStairs(int n) {
-        int dp[46] = {};
-        dp[0] = 1; dp[1] = 1;
-        for(int i = 2;i<=n;++i){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        vector<int> mem(n + 2,-1);
+        return getDistinctWays(n, mem);
     }
 };
