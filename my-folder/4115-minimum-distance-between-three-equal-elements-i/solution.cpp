dc67c60c7a62f322b1e1836259459c4a8a9e46d2
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-04-10 10:48:27 +0000

    [LeetCode Sync] Runtime - 3 ms (58.18%), Memory - 28.6 MB (99.54%)

---
+++ my-folder/4115-minimum-distance-between-three-equal-elements-i/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int min_ = INT_MAX;
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = i + 1; j < nums.size(); ++j) {
+                for(int k = j + 1; k < nums.size(); ++k) {
+                    if(nums[i] == nums[j] && nums[j] == nums[k]) {
+                        min_ = min((abs(i - j) + abs(j - k) + abs(k - i)), min_);
+                    }
+                }
+            }
+        }
+
+        return min_ == INT_MAX ? -1 : min_;
+    }
+};
