863a4b5251705e25a60c5a3afce4b933e71dc89c
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-04-13 11:00:05 +0000

    [LeetCode Sync] Runtime - 32 ms (44.88%), Memory - 107.4 MB (76.07%)

---
+++ my-folder/2362-minimum-rounds-to-complete-all-tasks/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> freq;
+        for (int task : tasks) {
+            freq[task]++;
+        }
+        
+        int rounds = 0;
+        for (auto const& [task, count] : freq) {
+            if (count == 1) {
+                return -1; 
+            }
+            
+            if (count % 3 == 0) {
+                rounds += count / 3;
+            } 
+            else {
+                rounds += count / 3 + 1;
+            }
+        }
+        
+        return rounds;
+    }};
