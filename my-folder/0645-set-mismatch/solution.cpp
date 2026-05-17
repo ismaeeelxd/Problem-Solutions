d8fa0c71570b1ee59d91bfb7a8bd97e7cc4c8667
Ismail Muhammed <ismailmd.dev@gmail.com>
2026-05-17 18:22:01 +0000

    [LeetCode Sync] Runtime - 10 ms (36.64%), Memory - 25.9 MB (58.36%)

--- my-folder/0645-set-mismatch/solution.cpp
+++ my-folder/0645-set-mismatch/solution.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> res(2,0);
-        int n = nums.size();
-        vector<int> freq(n+1,0);
+        vector<int> sol(2);
+        sol[0] = -1;
+        sol[1] = -1;
+        sort(nums.begin(), nums.end());
+        int n = nums.size(); // 10
+        int freq[10001] = {0};
+        for(int i = 0; i < nums.size(); ++i) {
+            int number = nums[i]; // 1
+            freq[number - 1]++;
+        }
+
         for(int i = 0; i < n; ++i) {
-            freq[nums[i]]++;
+            if(freq[i] == 2) {
+                sol[0] = i + 1;
+            }
+            if(freq[i] == 0) {
+                sol[1] = i + 1;
+            }
         }
-        for(int i = 1; i <= n; ++i) {
-            if(freq[i] == 0) res[1] = i;
-            if(freq[i] == 2) res[0] = i;  
-        }
-        return res;
+
+        return sol;
     }
 };
+
